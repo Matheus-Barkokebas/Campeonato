78fe1798b00c7f7ef39ab576a1a1cbccff855ce9
Matheus-Barkokebas <matheusbarkcaribe@gmail.com>
2024-11-24 22:17:57 -0300

    Inicio tela de logar

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ import json
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'CampF1Game2025'
 
+@app.route('/')
+def home():
+    return render_template('login.html')
 
+@app.route('/logarSite', methods=['POST'])
+def login():
+    emailLogar = request.form.get('emailCamp')
+    senhaLogar = request.form.get('senhaCamp')
+    return render_template('login.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=8080, debug=True)
